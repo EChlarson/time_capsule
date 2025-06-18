239b8d5a17c20761843ac40e50b6cd23a77120a5
kimah72 <min23005@byui.edu>
2025-06-17 20:47:48 -0600

    fixed server.js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
-const dotenv = require('dotenv');
-dotenv.config({ path: './config/env.js' });
+require('dotenv').config();
 
 const app = express();
 app.use(express.json());
